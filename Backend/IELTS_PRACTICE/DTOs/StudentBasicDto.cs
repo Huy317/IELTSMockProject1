155d8f7b56e4ed060ea1eed057fa6ddc182414a0
Nguyễn Phát Tài <167590892+TaiPhat25@users.noreply.github.com>
2025-08-11 21:12:34 +0700

    User and StudentMetaData DTOs

---
+++ Backend/IELTS_PRACTICE/DTOs/StudentBasicDto.cs
@@ -0,0 +1,10 @@
+﻿namespace IELTS_PRACTICE.DTOs
+{
+    public class StudentBasicDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public double TargetBandScore { get; set; }
+        public double CurrentBandScore { get; set; }
+    }
+}
