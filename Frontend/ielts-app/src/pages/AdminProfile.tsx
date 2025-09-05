780d618da4b73fc5e0fa50dd9867f28e73f65a28
eiuThanhDuy <duynguyen120504@gmail.com>
2025-09-05 14:38:52 +0700

    delete some dummy, edit profile route

--- Frontend/ielts-app/src/pages/AdminProfile.tsx
+++ Frontend/ielts-app/src/pages/AdminProfile.tsx
@@ -16,50 +16,6 @@ function AdminProfile() {
         </a>
       </div>
       <InforCard userId={currentUserId} />
-      {/* <div className="card">
-        <div className="card-body">
-          <h5 className="fs-18 pb-3 border-bottom mb-3">Education</h5>
-          <div className="education-flow">
-            <div className="ps-4 pb-3 timeline-flow">
-              <div>
-                <h6 className="mb-1">BCA - Bachelor of Computer Applications</h6>
-                <p>International University - (2004 - 2010)</p>
-              </div>
-            </div>
-            <div className="ps-4 pb-3 timeline-flow">
-              <div>
-                <h6 className="mb-1">MCA - Master of Computer Application</h6>
-                <p>International University - (2010 - 2012)</p>
-              </div>
-            </div>
-            <div className="ps-4 pb-0 timeline-flow">
-              <div>
-                <h6 className="mb-1">Design Communication Visual</h6>
-                <p>International University - (2012-2015)</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
-      {/* <div className="card mb-0">
-        <div className="card-body">
-          <h5 className="fs-18 pb-3 border-bottom mb-3">Experience</h5>
-          <div className="d-flex align-items-center mb-4">
-            <span className="bg-light border avatar avatar-lg text-gray-9 flex-shrink-0 me-3"><i className="isax isax-briefcase fw-bold"></i></span>
-            <div>
-              <h6 className="mb-1">Web Design & Development Team Leader</h6>
-              <p>Creative Agency - (2013 - 2016)</p>
-            </div>
-          </div>
-          <div className="d-flex align-items-center">
-            <span className="bg-light border avatar avatar-lg text-gray-9 flex-shrink-0 me-3"><i className="isax isax-briefcase fw-bold"></i></span>
-            <div>
-              <h6 className="mb-1">Project Manager</h6>
-              <p>CJobcy Technology Pvt.Ltd - (Present)</p>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
