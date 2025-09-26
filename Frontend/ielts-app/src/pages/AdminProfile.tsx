74b196767873be117ae8aa330ad661d8f9606d53
Nguyễn Phát Tài <167590892+TaiPhat25@users.noreply.github.com>
2025-09-26 11:17:04 +0700

    Minor Update

--- Frontend/ielts-app/src/pages/AdminProfile.tsx
+++ Frontend/ielts-app/src/pages/AdminProfile.tsx
@@ -1,6 +1,7 @@
 import InforCard from "../components/admin/InforCard";
 import { useParams } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import { NavLink } from "react-router-dom";
 
 function AdminProfile() {
   // const { userId } = useParams();
@@ -13,9 +14,9 @@ function AdminProfile() {
     <div>
       <div className="page-title d-flex align-items-center justify-content-between">
         <h5 className="fw-bold">My Profile</h5>
-        <a href="#" className="edit-profile-icon">
+        <NavLink to="/admin/settings" className="edit-profile-icon">
           <i className="isax isax-edit-2"></i>
-        </a>
+        </NavLink>
       </div>
       {/* <InforCard userId={currentUserId} /> */}
       {isAuthenticated && user ? (
